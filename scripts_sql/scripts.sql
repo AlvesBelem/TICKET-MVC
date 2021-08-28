a2000ddd3e2dc8d6f59ee49336cf8fa656a57bac
fhssouza <fhssouza@gmail.com>
2021-08-28 16:54:56 -0300

    Criando a Branch

--- scripts_sql/scripts.sql
+++ scripts_sql/scripts.sql
@@ -13,3 +13,9 @@ CREATE TABLE tb_client (
     email VARCHAR(100) NOT NULL,    
 );
 
+INSERT INTO tb_client (name, document, contact, address, phone, email) VALUES 
+('Maria Fernandes', '060.719.330-13', 'Maria', 'Av. Almirante Barroso, 1020 - Belem-Pa', '(91)98179-3030', 'maria@email.com'), 
+('Carlos Souza', '717.667.920-73', 'Carlos', 'Av. Presidente Vargas, 1050 - Belem-Pa', '(91)98080-3030', 'maria@email.com'), 
+('Fernanda Silva', '722.593.780-48', 'Fernanda', 'Av. Julio Cezar, 2030 - Belem-Pa', '(91)98080-3030', 'maria@email.com')
+
+
